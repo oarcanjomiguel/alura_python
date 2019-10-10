3d2c1c2e90b8d04a6fa64fc97696d9352cf2e831
Miguel Nakajima Marques <oarcanjomiguel@gmail.com>
2019-10-09 21:50:37 -0300

    Adicionado metodo transfere

--- oo/conta.py
+++ oo/conta.py
@@ -1,19 +1,28 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         # print("construindo objeto {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("O saldo de {} é: {}".format(self.titular, self.saldo))
+        print("O saldo de {} é: {}".format(self.__titular, self.__saldo))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        if (self.saldo - valor > 0 - self.limite):
-            self.saldo -= valor
+        if (self.__saldo - valor > 0 - self.__limite):
+            self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        """Retira o valor da conta que chama o metodo e deposita no conta declarada
+
+        :param destino:
+        :param valor:
+        """
+        self.saca(valor)
+        destino.deposita(valor)
 
 # TODO: escrever a classe
